20ba72e946113a4f141ea0be3b17523e1c8e0e6f
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2015-10-01 10:21:50 -0400

    adding files

--- basic.py
+++ basic.py
@@ -1,19 +1,27 @@
 import numpy as np
 import matplotlib.pyplot as plt
-# import astero as a
+import astero
 import george
 from george.kernels import ExpSquaredKernel
 
 def format_data():
 
-    # load asteroseismic data
-    data = a.astero()
-    m = data.bteff > 0
-    KID = data.bKID[m]
-    teff, t_err = data.bteff[m], data.bteff_err[m]
-    logg, l_err = data.blogg[m], .5*(data.blogg_errp[m] + data.blogg_errm[m])
-    feh, f_err = data.bfeh[m], data.bfeh_err[m]
-    age, a_err = data.bage[m], .5*(data.bage_errp[m] + data.bage_errm[m])
+#     # load asteroseismic data
+#     data = a.astero()
+#     m = data.bteff > 0
+#     KID = data.bKID[m]
+#     teff, t_err = data.bteff[m], data.bteff_err[m]
+#     logg, l_err = data.blogg[m], .5*(data.blogg_errp[m] + data.blogg_errm[m])
+#     feh, f_err = data.bfeh[m], data.bfeh_err[m]
+#     age, a_err = data.bage[m], .5*(data.bage_errp[m] + data.bage_errm[m])
+
+    data = astero.astero()
+    m = data.iteff > 0
+    KID = data.iKID[m]
+    teff, t_err = data.iteff[m], data.iteff_err[m]
+    logg, l_err = data.ilogg[m], .5*(data.ilogg_errp[m] + data.ilogg_errm[m])
+    feh, f_err = data.ifeh[m], data.ifeh_err[m]
+    age, a_err = data.iage[m], .5*(data.iage_errp[m] + data.iage_errm[m])
 
     # load rotation period data
     data = np.genfromtxt("garcia.txt").T
@@ -38,34 +46,47 @@ def format_data():
             agerr.append(a_err[i])
 
     # save results. FIXME: these need to be arrays
-    data = np.vstack((KID, p, perr, t, terr, l, lerr, f, ferr, a, aerr))
-    np.savetxt("data.txt", data.T)
+    KIDs, p, perr, t, terr, l, lerr, f, ferr, a, aerr = np.array(KIDs), \
+            np.array(periods), np.array(p_err), np.array(t), np.array(terr), \
+            np.array(l), np.array(lerr), np.array(f), np.array(ferr), \
+            np.array(ag), np.array(agerr)
+    data = np.vstack((KIDs, p, perr, t, terr, l, lerr, f, ferr, a, aerr))
+#     np.savetxt("data.txt", data.T)
+    np.savetxt("big_data.txt", data.T)
 
-    return np.array(KIDs), np.array(periods), np.array(p_err), np.array(t), \
-            np.array(terr), np.array(l), np.array(lerr), np.array(f), \
-            np.array(ferr), np.array(ag), np.array(agerr)
+#     return np.array(KIDs), np.array(periods), np.array(p_err), np.array(t), \
+#             np.array(terr), np.array(l, np.array(lerr), np.array(f), \
+#             np.array(ferr), np.array(ag), np.array(agerr)
 
-def make_prediction(period, teff):
+def make_prediction(period, teff, logg, feh):
     """
     Given the training set and a new rotation period, predict an age
     """
+
     # load data
     KID, p, perr, t, terr, l, lerr, f, ferr, a, aerr = \
-            np.genfromtxt("data.txt").T
+            np.genfromtxt("big_data.txt").T
+    m = (t < 6250) * (l > 4.2) * (p > 1)
+    KID, p, perr, t, terr, l, lerr, f, ferr, a, aerr = \
+            KID[m], p[m], perr[m], t[m], terr[m], l[m], lerr[m], f[m], \
+            ferr[m], a[m], aerr[m]
 
     # set your x, y z and hyperparameters
-    x, y, z, xerr, yerr, zerr = p, a, t, perr, aerr, terr
-    D = np.vstack((x, z)).T
-    A, lx, lz = 10, 50, 600
+    x, y, z, u, w, xerr, yerr, zerr, uerr, werr = \
+            p, a, t, l, f, perr, aerr, terr, lerr, ferr
+    D = np.vstack((x, z, u, w)).T
+    A, lx, lz, lu, lw = 10, 50, 600, .8, .3
 
     # GP prediction
-    k = A**2 * ExpSquaredKernel([lx**2, lz**2], ndim=2)
+    k = A**2 * ExpSquaredKernel([lx**2, lz**2, lu**2, lw**2], ndim=4)
     gp = george.GP(k)
     gp.compute(D, yerr)
 
-    xs = np.zeros((1, 2))
+    xs = np.zeros((1, 4))
     xs[0, 0] = period
     xs[0, 1] = teff
+    xs[0, 2] = logg
+    xs[0, 3] = feh
 
     mu, cov = gp.predict(y, xs)  # do the prediction
     v = np.diag(cov)**.5
@@ -77,11 +98,12 @@ def make_prediction_1D():
     """
     # load data
     KID, p, perr, t, terr, l, lerr, f, ferr, a, aerr = \
-            np.genfromtxt("data.txt").T
+            np.genfromtxt("big_data.txt").T
+#             np.genfromtxt("data.txt").T
 
     # set your x, y and hyperparameters
-    x, y, xerr, yerr = l, a, lerr, aerr
-    A, l = 10, .8
+    x, y, xerr, yerr = f, a, ferr, aerr
+    A, l = 10, .3
 
     # GP prediction
     k = A**2 * ExpSquaredKernel(l**2)
@@ -100,6 +122,9 @@ def make_prediction_1D():
 
 if __name__ == "__main__":
 
-    make_prediction_1D()
-#     p, perr = make_prediction(26, 5700)
-#     print p, perr
+#     format_data()
+#     assert 0
+#     make_prediction_1D()
+#     assert 0
+    p, perr = make_prediction(26, 5700, 4.4, 0)
+    print p, perr
