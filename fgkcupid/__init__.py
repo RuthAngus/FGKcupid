581de0b02cddc1b13aee7d52647d9053e81f9bf0
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2016-10-05 09:36:57 +0100

    adding packaging files

---
+++ fgkcupid/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from .fgkcupid import age_model
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='fgkcupid',
+      version='0.1',
+      description='Online dating for stars.',
+      url='http://github.com/RuthAngus/fgkcupid',
+      author='Ruth Angus',
+      author_email='ruthangus@gmail.com',
+      license='MIT',
+      packages=['fgkcupid'],
+      install_requires=["numpy", "matplotlib", "pandas", "os", "george",
+                        "h5py"],
+      zip_safe=False)
