5b21d8563bf078635ca31b4cd86a3d39632684db
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2016-10-05 22:30:59 +0100

    adding isochronal age

--- dev/star_age.py
+++ dev/star_age.py
@@ -2,19 +2,20 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
-# import matplotlib.pyplot as plt
-# from isochrones.dartmouth import Dartmouth_Isochrone
-# from isochrones import StarModel
+import matplotlib.pyplot as plt
+from isochrones.dartmouth import Dartmouth_Isochrone
+from isochrones import StarModel
 import pandas as pd
 import os
 import kplr
 import glob
-from fgkcupid import teff2bv
+from fgkcupid import teff2bv, age_model
 from kepler_data import load_kepler_data
 import acf
 
 DATA_DIR = "data"
 LC_DIR = "/Users/ruthangus/.kplr/data/lightcurves"
+RESULTS_DIR = "results"
 
 
 class star(object):
@@ -59,12 +60,12 @@ class star(object):
         """
 
         assert len(id) < 10, "ID must be a 9-digit KIC id."
-        id = str(int(id)).zfill(9)  # make sure id is in KIC format.
+        self.id = str(int(id)).zfill(9)  # make sure id is in KIC format.
 
         # KIC parameters.
         if not teff or logg or feh or kepmag:
             client = kplr.API()
-            star = client.star(id)
+            star = client.star(self.id)
             if not teff:
                 self.teff = (star.kic_teff, 50)
             if not logg:
@@ -80,7 +81,7 @@ class star(object):
         if not parallax:
             Gdata = pd.read_csv(os.path.join(DATA_DIR,
                                              "ruth_matched.csv"))
-            m = np.array(Gdata["kepid"]) == int(id)
+            m = np.array(Gdata["kepid"]) == int(self.id)
             self.parallax = (float(np.array(Gdata["parallax"])[m]),
                              float(np.array(Gdata["parallax_error"])[m]))
 
@@ -89,7 +90,7 @@ class star(object):
             # Search McQuillan catalogue.
             Rdata = pd.read_csv(os.path.join(DATA_DIR,
                                              "Table_1_Periodic.txt"))
-            m = np.array(Rdata["KID"]) == int(id)
+            m = np.array(Rdata["KID"]) == int(self.id)
             if len(Rdata["KID"][m]):
                 self.prot = (float(np.array(Rdata["Prot"])[m]),
                              float(np.array(Rdata["Prot_err"])[m]))
@@ -98,7 +99,7 @@ class star(object):
                 # Search Garcia catalogue.
                 Rdata = pd.read_csv(os.path.join(DATA_DIR,
                                                  "chaplin_garcia.csv"))
-                m = np.array(Rdata["kid"]) == int(id)
+                m = np.array(Rdata["kid"]) == int(self.id)
                 if len(Rdata["kid"][m]):
                     self.prot = (float(np.array(Rdata["period"])[m]),
                                  float(np.array(Rdata["period_err"])[m]))
@@ -109,20 +110,65 @@ class star(object):
         # If no rotation period exists download the light curve and
         # calculate acf.
         # load or download light curve.
-        if not prot:
-            fnames = glob.glob(os.path.join(LC_DIR, "{}/*fits".format(id)))
-            if not len(fnames):
-                print("Downloading light curve...")
-                client = kplr.API()
-                star = client.star(id)
-                star.get_light_curves(fetch=True, shortcadence=False)
-            fnames = glob.glob(os.path.join(LC_DIR, "{}/*fits".format(id)))
-            x, y, yerr = load_kepler_data(fnames)
+        result_file = os.path.join(RESULTS_DIR,
+                                   "{}_result.txt".format(self.id))
+        if os.path.exists(result_file):
+            print("Previous rotation period measurement found")
+            prot = tuple(np.genfromtxt(result_file))
+        else:
+            if not prot:
+                fnames = glob.glob(os.path.join(LC_DIR,
+                                                "{}/*fits".format(self.id)))
+                if not len(fnames):
+                    print("Downloading light curve...")
+                    client = kplr.API()
+                    star = client.star(self.id)
+                    star.get_light_curves(fetch=True, shortcadence=False)
+                fnames = glob.glob(os.path.join(LC_DIR,
+                                                "{}/*fits".format(self.id)))
+                x, y, yerr = load_kepler_data(fnames)
 
-            # calculate ACF
-            prot = acf.corr_run(x, y, yerr, id, savedir="results",
-                                saveplot=True)
-        print(prot)
+                # calculate ACF
+                self.prot = acf.corr_run(x, y, yerr, self.id,
+                                         savedir="results",
+                                         saveplot=True)
+
+        # Calculate a gyrochronology age using fgkcupid.
+        fname = os.path.join(RESULTS_DIR, "{}_gyro_age.txt".format(self.id))
+        if os.path.exists(fname):
+            self.gyro_age = tuple(np.genfromtxt(fname))
+        else:
+            print("Calculating rotation-age")
+            self.gyro_age = age_model(self.BV[0], self.prot[0], plot=True)
+            np.savetxt(fname, self.gyro_age)
+
+    def isochronal_age(self):
+        """
+        Calculate the isochronal age of the star using Tim Morton's isochrones
+        code.
+        """
+
+        fname = os.path.join(RESULTS_DIR, "{}.h5".format(self.id))
+        if os.path.exists(fname):
+            mod = StarModel.load_hdf(fname)
+        else:
+
+            # set maxage
+            maxage = self.gyro_age[0] + self.gyro_age[1]
+
+            # set up StarModel
+            dar = Dartmouth_Isochrone()
+            mod = StarModel(dar, Teff=self.teff, logg=self.logg, feh=self.feh,
+                            Kepler=self.kepmag, maxage=maxage, use_emcee=True)
+
+            # Calculating isochronal age. Running MCMC..."
+            mod.fit_mcmc()
+            mod.save_hdf(fname)
+
+        age_samples = mod.prop_samples("age")
+        age, p1, p2 = np.percentile(age_samples[0], [16, 50, 84])
+        age_errp, age_errm = p2 - age, age - p1
+        return age, age_errp, age_errm, age_samples
 
 
 def match(id1, id2):
@@ -146,4 +192,5 @@ def match(id1, id2):
 
 
 if __name__ == "__main__":
-    star("002450729")
+    st = star("002450729")
+    st.isochronal_age()
